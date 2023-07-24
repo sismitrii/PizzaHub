762f98fec5b5c05c39a970d6a691d829af064b65
Florian Guerin <floguerin156@gmail.com>
2023-07-24 16:01:39 +0200

    init

---
+++ src/main/java/fr/eni/demoSpring/PizzaHubApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.demoSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaHubApplication.class, args);
+	}
+
+}

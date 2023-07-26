d09064728f830cc0c95683591de6397e2efeeda5
Florian Guerin <floguerin156@gmail.com>
2023-07-26 17:25:17 +0200

    Update PizzaHubV2.sql

--- PizzaHub/assets/PizzaHubV2.sql
+++ PizzaHub/assets/PizzaHubV2.sql
@@ -1,78 +1,99 @@
 CREATE TABLE [Role] (
   [id] Int PRIMARY KEY IDENTITY(1, 1),
-  [name] String UNIQUE NOT NULL
+  [name] VARCHAR(50) UNIQUE NOT NULL
 )
 GO
 
 CREATE TABLE [MenuItemCategory] (
   [id] Int PRIMARY KEY IDENTITY(1, 1),
-  [name] String UNIQUE NOT NULL
+  [name] VARCHAR(50) UNIQUE NOT NULL
 )
 GO
 
 CREATE TABLE [Product] (
-  [id] Int PRIMARY KEY IDENTITY(1, 1),
-  [name] String NOT NULL,
+  [product_id] Int PRIMARY KEY IDENTITY(1, 1),
+  [name] VARCHAR(255) NOT NULL,
   [stock] Int DEFAULT (0)
 )
 GO
 
 CREATE TABLE [MenuItem] (
-  [id] Int PRIMARY KEY IDENTITY(1, 1),
-  [name] String NOT NULL,
-  [price] Double NOT NULL,
-  [category_id] Int NOT NULL,
-  [products] Int[] NOT NULL
+  [menu_item_id] Int PRIMARY KEY IDENTITY(1, 1),
+  [name] VARCHAR(255) NOT NULL,
+  [price] DECIMAL(10,2) NOT NULL,
+  [category_id] Int NOT NULL FOREIGN KEY REFERENCES [MenuItemCategory] ([id]),
+--  [products] Int[] NOT NULL -- TODOOOOOO
 )
 GO
 
+CREATE TABLE [Product_MenuItem] (
+  [product_id] Int FOREIGN KEY REFERENCES [Product]([product_id]),
+  [menu_item_id] Int FOREIGN KEY  REFERENCES [MenuItem]([menu_item_id]),
+  PRIMARY KEY ([product_id], [menu_item_id])
+);
+GO
+
 CREATE TABLE [Pizza] (
-  [id] Int PRIMARY KEY IDENTITY(1, 1),
-  [menu_item_id] Int UNIQUE NOT NULL,
-  [pizza_base_id] Int NOT NULL
+  [pizza_id] Int PRIMARY KEY IDENTITY(1, 1),
+  [menu_item_id] Int UNIQUE NOT NULL FOREIGN  KEY REFERENCES [MenuItem]([menu_item_id]),
+  [picture_url] VARCHAR(255)
 )
 GO
 
 CREATE TABLE [Order] (
-  [id] Int PRIMARY KEY IDENTITY(1, 1),
-  [is_completed] Bool NOT NULL DEFAULT (false)
+  [order_id] Int PRIMARY KEY IDENTITY(1, 1),
+  [is_completed] BIT NOT NULL DEFAULT (0)
 )
 GO
 
 CREATE TABLE [RestaurantOrder] (
-  [id] Int PRIMARY KEY IDENTITY(1, 1),
-  [order_id] Int UNIQUE NOT NULL,
+  [restaurantOrder_id] Int PRIMARY KEY IDENTITY(1, 1),
+  [order_id] Int UNIQUE NOT NULL FOREIGN KEY REFERENCES [Order]([order_id]) ,
   [table_number] Int NOT NULL,
   [seats] Int NOT NULL,
   [order_step] Int NOT NULL DEFAULT (1),
-  [menu_items] Int[] NOT NULL
+  --[menu_items] Int[] NOT NULL -- TODOOOOOO
 )
+GO
+
+CREATE TABLE [MenuItem_RestaurantOrder] (
+  [menuItem_id] Int FOREIGN  KEY REFERENCES [MenuItem]([menu_item_id]),
+  [restaurantOrder_id] Int FOREIGN KEY REFERENCES [RestaurantOrder]([restaurantOrder_id])
+  PRIMARY KEY ([menuItem_id], [restaurantOrder_id])
+);
 GO
 
 CREATE TABLE [TimeSlot] (
   [time_slot_id] Int PRIMARY KEY IDENTITY(1, 1),
-  [reserved] Bool NOT NULL DEFAULT (false),
+  [reservedPizza] Int NOT NULL DEFAULT (0),
   [slot] Datetime UNIQUE NOT NULL
 )
 GO
 
 CREATE TABLE [OnlineOrder] (
   [online_order_id] Int PRIMARY KEY IDENTITY(1, 1),
-  [order_id] Int UNIQUE NOT NULL,
-  [customer_name] String NOT NULL,
-  [time_slot_id] Int NOT NULL,
-  [pizzas] Int[] NOT NULL
+  [order_id] Int UNIQUE NOT NULL FOREIGN KEY REFERENCES [Order]([order_id]),
+  [customer_name] VARCHAR(255) NOT NULL,
+  [time_slot_id] Int NOT NULL FOREIGN KEY REFERENCES [TimeSlot]([time_slot_id]),
+  --[pizzas] Int[] NOT NULL -- TODOOOOOO
 )
+GO
+
+CREATE TABLE [Pizza_OnlineOrder] (
+  [pizza_id] Int FOREIGN KEY REFERENCES [Pizza]([pizza_id]),
+  [online_order_id] Int FOREIGN KEY REFERENCES [OnlineOrder]([online_order_id]),
+  PRIMARY KEY ([pizza_id], [online_order_id])
+);
 GO
 
 CREATE TABLE [Employee] (
   [employee_id] Int PRIMARY KEY IDENTITY(1, 1),
-  [first_name] String NOT NULL,
-  [last_name] String NOT NULL,
-  [password] Hash UNIQUE NOT NULL,
-  [email] String NOT NULL,
-  [puuid] String UNIQUE NOT NULL,
-  [role_name] Int NOT NULL
+  [first_name] VARCHAR(255) NOT NULL,
+  [last_name] VARCHAR(255) NOT NULL,
+  [password] VARCHAR(255) UNIQUE NOT NULL,
+  [email] VARCHAR(255) NOT NULL,
+  [puuid] CHAR(4) UNIQUE NOT NULL,
+  [role_id] Int NOT NULL FOREIGN KEY REFERENCES [Role]([id])
 )
 GO
 
@@ -117,64 +138,4 @@ EXEC sp_addextendedproperty
 GO
 
 ALTER TABLE [MenuItem] ADD FOREIGN KEY ([category_id]) REFERENCES [MenuItemCategory] ([id])
-GO
-
-CREATE TABLE [Product_MenuItem] (
-  [Product_id] Int,
-  [MenuItem_products] Int[],
-  PRIMARY KEY ([Product_id], [MenuItem_products])
-);
-GO
-
-ALTER TABLE [Product_MenuItem] ADD FOREIGN KEY ([Product_id]) REFERENCES [Product] ([id]);
-GO
-
-ALTER TABLE [Product_MenuItem] ADD FOREIGN KEY ([MenuItem_products]) REFERENCES [MenuItem] ([products]);
-GO
-
-
-ALTER TABLE [Pizza] ADD FOREIGN KEY ([menu_item_id]) REFERENCES [MenuItem] ([id])
-GO
-
-ALTER TABLE [Pizza] ADD FOREIGN KEY ([pizza_base_id]) REFERENCES [Product] ([id])
-GO
-
-ALTER TABLE [RestaurantOrder] ADD FOREIGN KEY ([order_id]) REFERENCES [Order] ([id])
-GO
-
-CREATE TABLE [MenuItem_RestaurantOrder] (
-  [MenuItem_id] Int,
-  [RestaurantOrder_menu_items] Int[],
-  PRIMARY KEY ([MenuItem_id], [RestaurantOrder_menu_items])
-);
-GO
-
-ALTER TABLE [MenuItem_RestaurantOrder] ADD FOREIGN KEY ([MenuItem_id]) REFERENCES [MenuItem] ([id]);
-GO
-
-ALTER TABLE [MenuItem_RestaurantOrder] ADD FOREIGN KEY ([RestaurantOrder_menu_items]) REFERENCES [RestaurantOrder] ([menu_items]);
-GO
-
-
-ALTER TABLE [OnlineOrder] ADD FOREIGN KEY ([order_id]) REFERENCES [Order] ([id])
-GO
-
-ALTER TABLE [OnlineOrder] ADD FOREIGN KEY ([time_slot_id]) REFERENCES [TimeSlot] ([time_slot_id])
-GO
-
-CREATE TABLE [Pizza_OnlineOrder] (
-  [Pizza_id] Int,
-  [OnlineOrder_pizzas] Int[],
-  PRIMARY KEY ([Pizza_id], [OnlineOrder_pizzas])
-);
-GO
-
-ALTER TABLE [Pizza_OnlineOrder] ADD FOREIGN KEY ([Pizza_id]) REFERENCES [Pizza] ([id]);
-GO
-
-ALTER TABLE [Pizza_OnlineOrder] ADD FOREIGN KEY ([OnlineOrder_pizzas]) REFERENCES [OnlineOrder] ([pizzas]);
-GO
-
-
-ALTER TABLE [Employee] ADD FOREIGN KEY ([role_name]) REFERENCES [Role] ([name])
 GO
